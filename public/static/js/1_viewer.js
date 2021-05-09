b5bd902a79eccae230a0e8c1dae515f18c9c2861
DiriiMQ <minhquang116@gmail.com>
2021-05-09 10:54:16 +0700

    sync

--- public/static/js/1_viewer.js
+++ public/static/js/1_viewer.js
@@ -17,16 +17,16 @@ socket.on("disconnect",function(){
 // })
 
 function b64EncodeUnicode(str) {
-  return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
-    function toSolidBytes(match, p1) {
-      return String.fromCharCode('0x' + p1);
-  }));
+	return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+		function toSolidBytes(match, p1) {
+		return String.fromCharCode('0x' + p1);
+	}));
 }
 
 function b64DecodeUnicode(str) {
-  return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-  }).join(''))
+	return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+		return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+	}).join(''))
 }
 
 
